b9869c36f863955f69940021cd8a8e2bc8c49239
John Paul Alcala <ore.wa.dragon.lord@gmail.com>
2016-03-29 17:15:57 +0800

    Created a python script to start in either consumer or producer mode.
    The docker image can now be run as either a consumer or in an
    elegant manner.
    
    This is pretty rough, but should get the job done.

--- k2sbridge/consumer.py
+++ k2sbridge/consumer.py
@@ -1,20 +1,21 @@
 from kafka import KafkaConsumer
 import stomp, logging, os
 
-logging.info("Starting consumer")
-assert os.getenv('KAFKA_TOPIC')
-consumer = KafkaConsumer(os.getenv('KAFKA_TOPIC'),
-                         bootstrap_servers=['%s:%s' % (os.getenv('KAFKA_HOST', 'localhost'), os.getenv('KAFKA_PORT', '9092'))])
+def start():
+    logging.info("Starting consumer")
+    assert os.getenv('KAFKA_TOPIC')
+    consumer = KafkaConsumer(os.getenv('KAFKA_TOPIC'),
+                             bootstrap_servers=['%s:%s' % (os.getenv('KAFKA_HOST', 'localhost'), os.getenv('KAFKA_PORT', '9092'))])
 
-logging.info("Starting producer")
-stomp_conn = stomp.Connection([(os.getenv('STOMP_HOST', 'localhost'), os.getenv('STOMP_PORT', '61613'))])
-stomp_conn.set_listener('', stomp.PrintingListener())
-stomp_conn.start()
-stomp_conn.connect(wait=True)
+    stomp_conn = stomp.Connection([(os.getenv('STOMP_HOST', 'localhost'), os.getenv('STOMP_PORT', '61613'))],
+                                  auto_decode=False))
+    stomp_conn.set_listener('', stomp.PrintingListener())
+    stomp_conn.start()
+    stomp_conn.connect(wait=True)
 
-logging.info("Waiting for data...")
-for data in consumer:
-    print(data.value)
-    stomp_conn.send(body=data.value, destination=os.getenv('STOMP_TOPIC'))
+    logging.info("Waiting for data...")
+    for data in consumer:
+        print(data.value)
+        stomp_conn.send(body=data.value, destination=os.getenv('STOMP_TOPIC'))
 
-logging.info("Shutting down...")
+    logging.info("Shutting down...")
