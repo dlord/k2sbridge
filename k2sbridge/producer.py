cd50d2e0d2fb141f7cfc84e476e97148fd7973da
John Paul Alcala <ore.wa.dragon.lord@gmail.com>
2016-06-27 22:33:13 +0800

    The producer script now uses KafkaProducer.
    SimpleProducer is not only a deprecated API, it is problematic
    when it comes to handling disconnects and retries.
    
    The KafkaProducer, while harder to setup, offers more features,
    including handling retries.
    
    The config I put here did not come from me. This was copied from
    another project which I know is working. For now, we'll stick
    to this. We can make this configurable later on.

--- k2sbridge/producer.py
+++ k2sbridge/producer.py
@@ -1,11 +1,31 @@
-from kafka import KafkaClient, SimpleProducer
+from kafka import KafkaProducer
 import stomp, logging, os, threading
 
+CLIENT_ID = os.getenv('KAFKA_CLIENT_ID', 'k2sbridge-producer')
+MAX_RETRY = int(os.getenv('KAFKA_MAX_RETRY', '3'))
 
 def start():
     logging.info('Starting producer')
-    kafka = KafkaClient('%s:%s' % (os.getenv('KAFKA_HOST', 'localhost'), os.getenv('KAFKA_PORT', '9092')))
-    producer = SimpleProducer(kafka)
+    server_address = '%s:%s' % (os.getenv('KAFKA_HOST', 'localhost'), os.getenv('KAFKA_PORT', '9092'))
+
+    kafka_opts_defaults = {
+        'client_id'                              : CLIENT_ID,
+        'retries'                                : MAX_RETRY,
+        'batch_size'                             : 16384,
+        'linger_ms'                              : 0,
+        'max_block_ms'                           : 60000,
+        'max_request_size'                       : 1048576,
+        'metadata_max_age_ms'                    : 300000,
+        'retry_backoff_ms'                       : 100,
+        'request_timeout_ms'                     : 30000,
+        'reconnect_backoff_ms'                   : 50,
+        'max_in_flight_requests_per_connection'  : 5,
+        'security_protocol'                      : 'PLAINTEXT',
+        'acks'                                   : 'all',
+        'bootstrap_servers'                      : server_address,
+    }
+
+    producer = KafkaProducer(**kafka_opts_defaults)
 
     class K2SBridgeListener(stomp.ConnectionListener):
         def on_error(self, headers, message):
@@ -13,7 +33,7 @@ def start():
 
         def on_message(self, headers, message):
             logging.info('received a message "%s"' % message)
-            producer.send_messages(os.getenv('KAFKA_TOPIC'), message)
+            producer.send(os.getenv('KAFKA_TOPIC'), value=message)
 
 
     stomp_conn = stomp.Connection([(os.getenv('STOMP_HOST', 'localhost'), os.getenv('STOMP_PORT', '61613'))],
